3dc13caceb2a7b985a4dc48aa225923ae3669f4a
Mark Colby <macolby14@gmail.com>
2024-09-11 11:15:48 -0400

    Wrap chart in its own component

--- src/App.tsx
+++ src/App.tsx
@@ -1,81 +1,10 @@
-import { useEffect, useState } from "react";
-import { IUVIndexData, parseRawUvData } from "@/parseUvData";
-import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
-import { format as formatDate } from "date-fns";
-import {
-  CartesianGrid,
-  Line,
-  ReferenceLine,
-  Tooltip,
-  XAxis,
-  YAxis,
-  LineChart,
-} from "recharts";
-
-const fetchUvData = async () => {
-  const response = await fetch(
-    "https://data.epa.gov/efservice/getEnvirofactsUVHOURLY/ZIP/10065/JSON"
-  ).then((data) => data.json());
-  return response;
-};
+import { UVIndexChart } from "./components/UVIndexChart";
 
 function App() {
-  const [uvData, setUvData] = useState<IUVIndexData[]>([]);
-  const [loading, setLoading] = useState(true);
-
-  const currentTimestamp = new Date().getTime();
-  useEffect(() => {
-    const loadData = async () => {
-      const rawData = await fetchUvData();
-      setLoading(false);
-      const formattedData = parseRawUvData(rawData);
-      setUvData(formattedData);
-    };
-
-    loadData();
-  }, []);
-
   return (
-    <Card className="w-full max-w-3xl mx-auto">
-      <CardHeader>
-        <CardTitle>UV Index for {formatDate(new Date(), "MMMM d")}</CardTitle>
-      </CardHeader>
-      <CardContent>
-        {loading ? (
-          <div className="flex justify-center items-center h-64">
-            Loading...
-          </div>
-        ) : (
-          <LineChart
-            width={600}
-            height={300}
-            data={uvData}
-            margin={{ top: 5, right: 30, left: 20, bottom: 5 }}
-          >
-            <CartesianGrid strokeDasharray="3 3" />
-            <XAxis
-              domain={uvData.map((data) => data.dateTime)}
-              minTickGap={0}
-              type="number"
-              scale="time"
-              dataKey="dateTime"
-              tickFormatter={(datetime) =>
-                `${new Date(datetime).getHours()}:00`
-              }
-            />
-            <YAxis />
-            <Tooltip
-              labelFormatter={(datetime) =>
-                `Time: ${new Date(datetime).getHours()}:00`
-              }
-              formatter={(value) => [`UV Index: ${value}`]}
-            />
-            <Line dataKey="uvValue" type="monotone" stroke="#8884d8" />
-            <ReferenceLine x={currentTimestamp} stroke="red" label="Now" />
-          </LineChart>
-        )}
-      </CardContent>
-    </Card>
+    <div className="p-4">
+      <UVIndexChart />
+    </div>
   );
 }
 
